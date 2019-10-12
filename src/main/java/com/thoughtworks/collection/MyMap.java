d79b7acd1610e113fba7d0161c5a51d237357b5e
Dan Luigi Tating <danluigitating@hotmail.com>
2019-10-12 13:44:16 +0800

    MyMap

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -3,7 +3,10 @@ package com.thoughtworks.collection;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class MyMap {
 
@@ -17,11 +20,15 @@ public class MyMap {
     }
 
     public List<Integer> getTriple() {
-        throw new NotImplementedException();
+        return array.stream()
+                .map(element -> element *3)
+                .collect(Collectors.toList());
     }
 
     public List<String> mapLetter() {
-        throw new NotImplementedException();
+        return IntStream.range(0, array.size())
+                .mapToObj(i -> letters[i])
+                .collect(Collectors.toList());
     }
 
     public List<String> mapLetters() {
@@ -29,10 +36,14 @@ public class MyMap {
     }
 
     public List<Integer> sortFromBig() {
-        throw new NotImplementedException();
+      return array.stream()
+              .sorted(Comparator.reverseOrder())
+              .collect(Collectors.toList());
     }
 
     public List<Integer> sortFromSmall() {
-        throw new NotImplementedException();
+        return array.stream()
+                .sorted()
+                .collect(Collectors.toList());
     }
 }
